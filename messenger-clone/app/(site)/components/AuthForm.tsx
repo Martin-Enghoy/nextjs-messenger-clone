79b675f02185d63b96d11c13cf0a97a73b74471f
Martin Adrian Enghoy <adrianenghoy@gmail.com>
2023-08-07 15:59:25 +0800

    initialized auth pages, created emptystate component

--- messenger-clone/app/(site)/components/AuthForm.tsx
+++ messenger-clone/app/(site)/components/AuthForm.tsx
@@ -2,7 +2,7 @@
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 
@@ -10,11 +10,21 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      console.log("Authenticated!");
+      router.push("/users");
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
@@ -43,6 +53,7 @@ const AuthForm = () => {
       //AXIOS REGISTER
       axios
         .post("/api/register", data)
+        .then(() => signIn("credentials", data))
         .catch(() => toast.error("Something went wrong!"))
         .finally(() => setIsLoading(false));
     }
@@ -60,6 +71,7 @@ const AuthForm = () => {
 
           if (callback?.ok && !callback?.error) {
             toast.success("Successfully logged in!");
+            router.push("/users");
           }
         })
         .finally(() => setIsLoading(false));
